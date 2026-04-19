c07eebddf227b83154c1d4d7435df69ff5b86d99
Neelu <scalevyapar072@gmail.com>
2026-04-19 12:34:32 +0530

    reduce prompt strength to fix mannequin

--- app/api/vizora/generate/route.ts
+++ app/api/vizora/generate/route.ts
@@ -1,4 +1,4 @@
-﻿import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import Replicate from 'replicate'
 
 export async function POST(req: NextRequest) {

0455fd36461e55b9a19be24feecced1b00aac736
scalevyapar072-svg <scalevyapar072@gmail.com>
2026-05-11 22:05:38 +0530

    Update page.tsx

--- app/labour/company/panel/page.tsx
+++ app/labour/company/panel/page.tsx
@@ -1,16 +1,12 @@
-import { CompanySiteShell } from '../company-site-shell'
 import styles from '../company-site.module.css'
-import { getLabourCompanyWebsiteContent } from '@/lib/labour-company-website'
 import { CompanyPanelClient } from './company-panel-client'
 
 export default async function LabourCompanyPanelPage() {
-  const { content } = await getLabourCompanyWebsiteContent()
-
   return (
-    <CompanySiteShell content={content} currentPath="/labour/company/signin">
-      <section className={styles.stack}>
+    <div className={styles.page}>
+      <div className={styles.container}>
         <CompanyPanelClient />
-      </section>
-    </CompanySiteShell>
+      </div>
+    </div>
   )
 }
